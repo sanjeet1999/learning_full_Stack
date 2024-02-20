842ee4528400107a8e22bbcf8cc697189a3b4734
sanjeet1999 <sanjeetkr987152@gmail.com>
2024-02-21 00:09:00 +0530

    Zod input validation library

---
+++ zod_input_validation.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const zod = require("zod")
+const schema = zod.array(zod.number());
+app.use(express.json())
+
+app.post("/enter_array",(req,res)=>{
+    const arr = req.body.arr;
+    console.log(arr)
+// zod will through error 
+    const got_array = schema.safeParse(arr)
+    res.send(got_array)
+})
+app.listen(3000)
