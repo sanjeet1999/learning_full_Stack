224a1621f750fb704fd28739312cbf066a70079c
sanjeet1999 <sanjeetkr987152@gmail.com>
2024-02-24 17:54:44 +0530

    employee data validation

---
+++ zod_user_credential_validation.js
@@ -0,0 +1,26 @@
+// WAP to validate the users data(username = string,employid = id,Gender = "M/F",BMI = in decimal )
+const exp = require("express")
+const app = exp()
+const z = require("zod")
+
+const schema = z.object({
+    username:z.string(),
+    empid : z.number(),
+    empGender: z.literal("M").or(z.literal("F")),
+    empBMI : z.number().multipleOf(0.1)
+})
+app.use(exp.json())
+
+app.post("/user_credential",(req,res)=>{
+    const emp_data = req.body.user_data
+    const emp_clean_data = schema.safeParse(emp_data)
+    console.log(emp_data)
+    if (!emp_clean_data.success){
+        res.status(411).json({msg:"Input data is invalid"})
+    }
+    else{
+        res.send(emp_clean_data.data)
+    }
+})
+
+app.listen(3000)
